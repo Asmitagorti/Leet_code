59b545b2a01322bfa021c009ef706c14375d8f15
Asmita gorti <asmileis4evr@gmail.com>
2025-01-27 18:55:12 +0530

    Time: 10 ms (0.94%), Space: 10.8 MB (5.16%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
+    int freq[100000]={0};
+    int pro[100000]={0};
+    int *arr=(int*)malloc(numsSize*sizeof(int));
+    int index=0;
+    for(int i=0;i<numsSize;++i){
+        freq[nums[i]]++;
+    }
+    for(int i=0;i<numsSize;++i){
+        if(freq[nums[i]]>1 && !pro[nums[i]]) {
+            arr[index++]=nums[i];
+            pro[nums[i]]=1;
+        }
+    }
+    *returnSize=index;
+    return arr;
+}
