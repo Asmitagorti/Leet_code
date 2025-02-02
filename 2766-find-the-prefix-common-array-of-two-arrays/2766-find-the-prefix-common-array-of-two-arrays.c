d49248c3992f8cb11f74868e86d8d890d228416f
Asmita gorti <asmileis4evr@gmail.com>
2025-02-02 11:26:32 +0530

    Time: 0 ms (100%), Space: 14.4 MB (44.54%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.c
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 int* findThePrefixCommonArray(int* A, int ASize, int* B, int BSize, int* returnSize) {
     int* C = (int*)malloc(ASize * sizeof(int));
-    int commonCount = 0;
-    *returnSize = ASize;
 
-    // Iterate through all indices of the arrays
+    int seenA[10001] = {0};  
+    int seenB[10001] = {0};  
+
     for (int i = 0; i < ASize; ++i) {
-        commonCount = 0; // Reset count for each prefix
+        seenA[A[i]] = 1;
+        seenB[B[i]] = 1;
+        int c = 0;
         for (int j = 0; j <= i; ++j) {
-            // Check if current elements in A and B are equal
-            for (int k = 0; k <= i; ++k) {
-                if (A[j] == B[k]) {
-                    commonCount++;
-                    break;
-                }
+            if (seenA[A[j]] && seenB[A[j]]) {
+                c++;
             }
         }
-        C[i] = commonCount;
+
+        C[i] = c;
     }
+    *returnSize = ASize;
     return C;
 }
 
