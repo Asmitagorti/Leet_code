79f83a48aa099d0ed8676566233b3b870f1b5e29
Asmita gorti <asmileis4evr@gmail.com>
2025-02-08 07:24:09 +0530

    Time: 0 ms (100%), Space: 14.5 MB (26.17%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.c
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.c
@@ -1,11 +1,11 @@
-int peakIndexInMountainArray(int* arr, int arrSize) {
-    int max=arr[0];
-    int res;
-    for(int i=0;i<arrSize;++i){
-        if(arr[i]>max){
-            max=arr[i];
-            res=i;
+int peakIndexInMountainArray(int* arr, int arrSize) {// 0(logn) complexity..
+    int left=0,right=arrSize-1;
+    while(left<right){
+        int mid=left+(right-left)/2;
+        if(arr[mid]<arr[mid+1]){
+            left=mid+1;
         }
+        else right=mid;
     }
-    return res;
+    return left;
 }
