0038ecabb0efb018fa91654d19ae697e9086a2ca
Asmita gorti <asmileis4evr@gmail.com>
2025-02-11 06:44:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (35.71%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.c
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.c
@@ -1,14 +1,15 @@
 char* removeOccurrences(char* s, char* part) {
-    // strstr() finds the first occurence of a character
-    //  memove(), moves a block of code from the source to the destination,
-    // even when the memory overlaps
-    // Arguments: memmove(destination, source, number_of_bytes)
-    // It copies n bytes from source to destination, even if they overlap.
+    int len_s = strlen(s);
     int len_part = strlen(part);
-    char* pos;
-    while ((pos = strstr(s, part)) != NULL) {
-        memmove(pos, pos + len_part, strlen(pos + len_part)+1);
-    }
+    
+    int j = 0;  
+    for (int i = 0; i < len_s; i++) {
+        s[j++] = s[i];  
 
+        if (j >= len_part && strncmp(s + j - len_part, part, len_part) == 0) {
+            j -= len_part;  
+        }
+    }
+    s[j] = '\0';
     return s;
 }
