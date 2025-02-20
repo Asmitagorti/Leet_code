002663c2769ce855d8565eaf318df08996a5b3e2
Asmita gorti <asmileis4evr@gmail.com>
2025-02-20 13:25:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (33.56%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -0,0 +1,23 @@
+int lengthOfLongestSubstring(char* s) {
+    // sliding windows concept --> maintaining a specific size of a substring in a string, and then 'sliding' that window, across the length of the string
+    // time complexity --> O(N)
+    int left=0, right=0, max_length=0,freq[256]={0};//
+    int len=strlen(s), cur = 0;
+    if(len==0) return 0;
+    while(right<len){
+        freq[s[right]]++;
+        while(freq[s[right]]>1){
+            freq[s[left]]--;
+            left++;
+        }
+        cur=right-left+1; // find the current length
+        if(cur>max_length){
+            max_length=cur;
+        }
+        right++;
+    }
+    if(cur>max_length){
+            max_length=cur;
+        }
+    return max_length;
+}
