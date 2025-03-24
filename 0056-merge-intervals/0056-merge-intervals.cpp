a7af2b509d3f487ab8ee1090f13d48441303567a
Asmita gorti <asmileis4evr@gmail.com>
2025-03-24 22:47:15 +0530

    Time: 8 ms (29.27%), Space: 25 MB (11.78%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -10,22 +10,28 @@ public:
         int prev_end=-1, prev_start=-1;
         vector<vector<int>>merged;
         for(auto &[cur_start,cur_end]:hi){ // removes the unecessary copying
-        // cur_start =
             if(prev_start==-1) {// for the 1st iteration..
                 prev_start=cur_start;
                 prev_end=cur_end;
             }
             else if(prev_end>=cur_start){
                 prev_end=max(prev_end,cur_end);
-
+                // for {1,3},{2,6}
+                // 3 is >= 2, so 2 belongs within its range
+                // so for the key prev_start, the value is max(3,6), which is 6
+                // so finally its: {1->6};
             }
             else{
                 merged.push_back({prev_start,prev_end});
                 prev_start=cur_start;
                 prev_end=cur_end;
+                // if there is the values dont fall into another range
+                // we store the previous pair into merged
+                // and then re assign prev_start, and prev_end
             }
         }
         merged.push_back({prev_start,prev_end});
+        // we push the last pairs into merged
         return merged;
     }
 };
