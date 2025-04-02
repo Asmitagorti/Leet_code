475dca96333b0788543ae88f057f66e3a8bb1156
Asmita gorti <asmileis4evr@gmail.com>
2025-04-02 07:00:53 +0530

    Time: 0 ms (100%), Space: 21.5 MB (88.55%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
+typedef long long ll;
     long long maximumTripletValue(vector<int>& nums) {
-        long long n=nums.size(),max=-1;
-        for(long long i=0;i<n;++i){
-            for(long long j=i+1;j<n;++j){
-                for(long long k=j+1;k<n;++k){
-                    long long z=nums[i]-nums[j];
-                    long long res=z*nums[k];
-                    if(res>max) max=res;
-                }
-            }
+        ll x=nums[0],y=0,z=0;
+        int n=nums.size();
+        for(ll i=1;i<n-1;++i){
+            x=max(x,ll(nums[i]));
+            // cout<<x<<endl;
+            y=max(y,x-ll(nums[i]));
+            // cout<<y<<endl;
+            z=max(z,y*ll(nums[i+1]));
+            // cout<<z<<endl;
         }
-        if(max==-1) return 0;
-        return max;
+        return z;
     }
 };
