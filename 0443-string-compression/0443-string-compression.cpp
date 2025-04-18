f70cb95f6db92fd362219dab802f16397909e3ad
Asmita gorti <asmileis4evr@gmail.com>
2025-04-18 12:44:20 +0530

    Time: 0 ms (100%), Space: 13.6 MB (73.52%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int ind=0;
+        int i=0;
+        while(i<chars.size()){
+            int ct=1;
+            char ch=chars[i];
+            int j=i+1;
+            while(j<chars.size() && ch==chars[j]){
+                ct++;
+                j++;
+            }
+            chars[ind++]=ch;
+            if(ct>1){
+                string num=to_string(ct);
+                for(auto hi:num){
+                    chars[ind++]=hi;
+                }
+            }
+            i=j;
+        }
+        return ind;
+    }
+};
