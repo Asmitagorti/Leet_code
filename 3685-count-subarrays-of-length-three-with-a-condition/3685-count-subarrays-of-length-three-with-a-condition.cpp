bd42e141b37b9280c08ac9c243773c56c3c13784
Asmita gorti <asmileis4evr@gmail.com>
2025-04-27 16:45:08 +0530

    Time: 0 ms (100%), Space: 48.3 MB (95.92%) - LeetHub

---
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        int ct=0;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]+nums[i+2]==(nums[i+1]/2.0)) ct++;
+        }
+        return ct;
+    }
+};
