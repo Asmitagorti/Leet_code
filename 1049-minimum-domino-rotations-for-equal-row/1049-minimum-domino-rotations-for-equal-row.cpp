84484eca164cbc0cfa62a81542a87db5b52fb58c
Asmita gorti <asmileis4evr@gmail.com>
2025-05-03 19:21:47 +0530

    Time: 0 ms (100%), Space: 115.4 MB (48.71%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,54 +1,43 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int n = tops.size(),flag=0,res=0;
-        map<int, int> freq1;
-        map<int, int> freq2;
-        for (auto i : tops) {
-            freq1[i]++;
-        }
-        int max1 = -1, no1 = 0;
-        for (auto i : freq1) {
-            if (i.second > max1) {
-                max1 = i.second;
-                no1 = i.first;
+        int n = tops.size();
+        int res = INT_MAX;
+        int puppy = tops[0];
+        int kitty = bottoms[0];
+
+        int top_ct1 = 0, bottom_ct1 = 0;
+        int flag1 = 1;
+        for (int i = 0; i < n; i++) {
+            if (tops[i] != puppy && bottoms[i] != puppy) {
+                flag1 = 0;
+                break;
             }
+            if (tops[i] != puppy) top_ct1++;
+            if (bottoms[i] != puppy) bottom_ct1++;
         }
-        for (auto i : bottoms) {
-            freq2[i]++;
+        if (flag1 == 1) {
+            res = min(res, min(top_ct1, bottom_ct1));
         }
-        int max2 = -1, no2 = 0;
-        for (auto i : freq2) {
-            if (i.second > max2) {
-                max2 = i.second;
-                no2 = i.first;
-            }
-        }
-        // u know have the max repeated element in BOTH arrays
-        if (max1 > max2) {
-            int ct1=0;
-            for (int j = 0; j < n; ++j) {
-                if (tops[j]!=bottoms[j] && bottoms[j] == no1) {
-                    ct1++;
+        if (puppy != kitty) {
+            int top_ct2 = 0, bottom_ct2 = 0;
+            int flag2 = 1;
+
+            for (int i = 0; i < n; i++) {
+                if (tops[i] != kitty && bottoms[i] != kitty) {
+                    flag2 = 0;
+                    break;
                 }
+                if (tops[i] != kitty) top_ct2++;
+                if (bottoms[i] != kitty) bottom_ct2++;
             }
-            if (ct1 + max1 == n) {
-                res=ct1;
-                flag=1;
-            }
-        } else {
-            int ct2=0;
-            for (int j = 0; j < n; ++j) {
-                if (tops[j]!=bottoms[j] && tops[j] == no2) {
-                    ct2++;
-                }
-            }
-            if (ct2 + max2 == n) {
-                res=ct2;
-                flag=1;
+            if (flag2 == 1) {
+                res = min(res, min(top_ct2, bottom_ct2));
             }
         }
-        if(flag==1) return res;
-        else return -1;
+        if (res == INT_MAX)
+            return -1;
+        else
+            return res;
     }
 };
