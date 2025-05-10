e6752b08a900adf44debfb3053458f886e5e18be
Asmita gorti <asmileis4evr@gmail.com>
2025-05-10 23:08:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (5.63%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.c
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.c
@@ -2,7 +2,7 @@ int isvowel(char ch){
     return (ch=='a' || ch=='e' ||ch=='i' ||ch=='o' ||ch=='u');
 }
 int maxFreqSum(char* s) {
-    int freq_vowel[1001]={0}, freq_cons[1001]={0};
+    int freq_vowel[123]={0}, freq_cons[123]={0};
     for(int i=0;s[i]!='\0';++i){
         char ch=s[i];
         if(isvowel(ch)){
@@ -11,10 +11,8 @@ int maxFreqSum(char* s) {
         else freq_cons[ch]++;
     }
     int hii=-1, puppy=-1;
-    for(int i=0;i<1001;++i){
+    for(int i='a';i<='z';++i){
         if(freq_vowel[i]>hii) hii=freq_vowel[i];
-    }
-    for(int i=0;i<1001;++i){
         if(freq_cons[i]>puppy) puppy=freq_cons[i];
     }
     return hii+puppy;
