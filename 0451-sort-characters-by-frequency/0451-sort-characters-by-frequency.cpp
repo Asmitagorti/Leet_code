9751ab4b8b19fa7e66c08b791a5e530889cb6168
Asmita gorti <asmileis4evr@gmail.com>
2025-06-06 12:54:16 +0530

    Time: 945 ms (5%), Space: 10.8 MB (87.51%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        // most frequent first
+        map<char,int>hi;
+        for(auto i: s){
+            hi[i]++;
+            cout<<i<<" "<<hi[i]<<" "<<endl;
+        }
+        auto kitty=[](auto &a, auto &b){
+            return a.second>b.second;
+        };
+
+        // e:2, r:1, t:1
+        vector<pair<char,int>>pup(hi.begin(),hi.end());
+        // sort(pup.begin(), pup.end()); // sorts lexicographically
+        sort(pup.begin(), pup.end(),kitty);
+
+        string res;
+        for(auto i: pup){
+            for(int j=0;j<i.second;++j){
+                res.push_back(i.first);
+            }
+        }
+        return res;
+    }
+};
