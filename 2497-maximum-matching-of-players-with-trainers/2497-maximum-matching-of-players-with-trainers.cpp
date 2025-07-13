c3693b48e6d19e7c76b278b85bf111982e24be9f
Asmita gorti <asmileis4evr@gmail.com>
2025-07-13 12:22:22 +0530

    Time: 32 ms (31.16%), Space: 80.2 MB (85.54%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        // you dont need to give min(players)->max(trainers)
-        // u can use bin search, to find the min greater element in trainers than players
-        // and then we can remove that element from trainers
-        int ct=0,i=0;
-        sort(players.begin(),players.end());
-        multiset<int>s(trainers.begin(), trainers.end());
-        while(i<players.size()){
-            auto it= s.lower_bound(players[i]);
-            if(it!=s.end()) {
-                s.erase(it);
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        int i=0,j=0, ct=0;
+        while(i<players.size() && j<trainers.size()){
+            if(players[i]<=trainers[j]){
                 ct++;
+                i++;
+                j++;
             }
-            i++;
+            else j++;
         }
         return ct;
     }
