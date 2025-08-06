e407a904b56d68adcf9222632e1d5e7071349547
Asmita gorti <asmileis4evr@gmail.com>
2025-08-06 13:26:59 +0530

    Time: 24 ms (78.85%), Space: 17.4 MB (55.51%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>&vis,vector<vector<char>>&grid, int i, int j){
+        int n=grid.size();
+        int sum=0;
+        int m=grid[0].size();
+        if(i< 0|| i>=n || j>=m || j<0 ||grid[i][j]=='0' || vis[i][j]==1) return;
+        // if(grid[i][j]=='0') return;
+
+        vis[i][j]=1; // mark
+        // i-1,j
+        // i,j-1
+        // i+1,j
+        // i,j+1
+        dfs(vis,grid,i-1,j);
+        dfs(vis,grid,i,j-1);
+        dfs(vis,grid,i+1,j);
+        dfs(vis,grid,i,j+1);
+        
+    }
+    int numIslands(vector<vector<char>>& grid) {
+         // u have to keep searching till ur out of 1's or smthg
+        // basically, u need to check the number of 1's that have
+        // 0's on all 4 sides
+
+        // i need to find a way where i just check one 1, and
+        // ct++, and then skip to the next island (if any)
+
+        // constraints are less, max no of searches == 300*300
+        // == 9*10^4 -> 10^4, we're good
+        int n=grid.size();
+        int m=grid[0].size();
+        int ct=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    dfs(vis,grid,i,j);
+                    ct++;
+                } 
+            }
+        }
+        return ct;
+    }
+};
