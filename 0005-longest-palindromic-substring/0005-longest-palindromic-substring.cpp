a5b7bab09a6e0cb6e8dda37f8f6ae216494b545b
Asmita gorti <asmileis4evr@gmail.com>
2025-09-06 10:58:06 +0530

    Time: 19 ms (55.06%), Space: 27.5 MB (29.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,61 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        string str;
-        int len= s.size();
-        int i=0,j=0,max=0;
+    string dp[1001];
+    string solve(string s){
+        int i=0,j=0;
+        string str = "",res="";
+        int len=s.size();
+        int maxi=0;
         for(int k=0;k<len;++k){
-            i=k;
-            j=k+1;
-            while(i>=0 && j<=len && s[i]==s[j]){
-                int cur = j-i+1;
-                if(cur>max){
-                    max= cur;
-                    str = s.substr(i,cur);
-                }
-                i--;
-                j++;
-            }
             i=k;
             j=k;
             while(i>=0 && j<=len && s[i]==s[j]){
-                int cur = j-i+1;
-                if(cur>max){
-                    max = cur;
-                    str = s.substr(i,cur);
+                int cur=j-i+1;
+                if(cur>maxi){
+                    maxi=cur;
+                    str=s.substr(i,cur);
                 }
                 i--;
                 j++;
             }
+            int l=str.size();
+            if(dp[l].empty()){
+                dp[l]= str;
+            }
+            i=k;
+            j=k+1;
+            while(i>=0 && j<=len && s[i]==s[j]){
+                int cur=j-i+1;
+                if(cur>maxi){
+                    maxi=cur;
+                    str=s.substr(i,cur);
+                }
+                i--;
+                j++;
+            }
+            l=str.size();
+            if(dp[l].empty()){
+                dp[l] = str;
+            }
         }
-        return str
-        ;
+        for(int i=1000;i>=0;--i){
+            if(dp[i].empty()){
+                continue;
+            }
+            else{
+                res=dp[i];
+                break;
+            }
+        }
+        return res;
+    }
+    string longestPalindrome(string s) {
+        // dp
+        // recursive dp, cuz iterative dp needs a base case
+        string r = solve(s);
+        return r;
+
+
+                                                                   
     }
 };
