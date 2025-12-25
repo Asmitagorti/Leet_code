0ce5233964fd55ccca6d03677b00883428c02bac
Asmita gorti <asmileis4evr@gmail.com>
2025-12-25 17:33:45 +0530

    Time: 222 ms (9.93%), Space: 110.2 MB (18.09%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -7,7 +7,7 @@ public:
         // 2 3 4 5, k=1
         // 5 4 3 2, k=1
         for(int i=0;i<k;++i){
-            cout<<happiness[i]<<" ";
+            // cout<<happiness[i]<<" ";
             sum+=max(0,happiness[i]-i);
         }
         return sum;
